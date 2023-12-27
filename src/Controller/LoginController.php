5f97ca8371ac0be14967aad1c488f26da5d203d1
melvin-crypto <melvin.pierre.mp@gmail.com>
2023-12-27 18:47:53 +0100

    Fix component + svg

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -19,10 +19,9 @@ class LoginController extends AbstractController
     ): Response {
 
         $user = new User();
-        // get the login error if there is one
+
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         $form = $this->createForm(
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,6 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
