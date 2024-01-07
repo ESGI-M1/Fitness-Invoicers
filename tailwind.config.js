744449141e716da3491a98768f634bf3c001145a
Jules-Rabus <jules200204@gmail.com>
2024-01-07 13:50:59 +0100

    rework navbar : darkmode, burger

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#00A76F',
+        primaryLighter: '#C8FAD6',
+        grey: '#797979',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
@@ -14,4 +21,3 @@ module.exports = {
     require('@tailwindcss/container-queries'),
   ],
 }
-
