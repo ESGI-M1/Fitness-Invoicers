07a581fd321e7c16ab2069e37ea50bc3ecdc7cf4
melvin-crypto <melvin.pierre.mp@gmail.com>
2024-01-14 17:34:28 +0100

    Add child for registration type

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,7 +19,22 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'label' => 'Nom',
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true,
+                'label' => 'Prénom',
+            ])
+            ->add('company', TextType::class, [
+                'required' => true,
+                'label' => 'Entreprise',
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => 'Email',
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
                 'constraints' => [
