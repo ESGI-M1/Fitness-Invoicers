893c80adff717f8f37dabd08b85ca9f2f7835fb4
melvin-crypto <melvin.pierre.mp@gmail.com>
2024-01-14 17:37:15 +0100

    Refont file config tailwind js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/container-queries'),
-  ],
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {
+            transitionProperty: {
+                'top-menu': 'margin-left',
+                'left-menu': 'left',
+            },
+            transitionDuration: {
+                '2': '2s',
+            },
+            transitionTimingFunction: {
+                'ease': 'ease',
+            },
+        },
+    },
+    plugins: [
+        function ({addUtilities}) {
+            const newUtilities = {
+                '.show-menu': {
+                    'left': '0 !important',
+                },
+            };
+            addUtilities(newUtilities, ['responsive', 'hover']);
+        },
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/container-queries'),
+    ],
 }
-
