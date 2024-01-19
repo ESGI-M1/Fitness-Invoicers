3d58d4c88fb3e24740db0b2187748b1059b3509f
Jules Rabus <jules200204@gmail.com>
2024-01-19 16:35:57 +0100

    Début fix route

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -19,56 +19,21 @@ class DashboardController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        // TODO : mettre en place une vrai entreprise par défaut
+        $user = $this->getUser();
+        $companies = $user->getcompanyMemberships()->getValues();
+        $company = $companies[0]->getCompany();
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'company' => $company,
         ]);
     }
 
-    #[Route('/utilisateurs', name: 'app_users')]
-    public function users(EntityManagerInterface $entityManager): Response
+    #[Route('/app_default_dev', name: 'app_default_dev')]
+    public function app_default_dev(): Response
     {
-        $users = $entityManager->getRepository(User::class)->findAll();
 
-        return $this->render('dashboard/users.html.twig', [
-            'users' => $users,
-        ]);
-    }
-
-    #[Route('/produits', name: 'app_products')]
-    public function products(EntityManagerInterface $entityManager): Response
-    {
-        $products = $entityManager->getRepository(Product::class)->findAll();
-
-        return $this->render('dashboard/products.html.twig', [
-            'products' => $products,
-        ]);
-    }
-
-    #[Route('/categories', name: 'app_categories')]
-    public function categories(EntityManagerInterface $entityManager): Response
-    {
-        $categories = $entityManager->getRepository(Category::class)->findAll();
-
-        return $this->render('dashboard/categories.html.twig', [
-            'categories' => $categories,
-        ]);
-    }
-
-    #[Route('/devis', name: 'app_quotes')]
-    public function quotes(EntityManagerInterface $entityManager): Response
-    {
-        $users = null;
-        $quotes = $entityManager->getRepository(Quote::class)->findBy([$users => $users]);
-
-        return $this->render('dashboard/quotes.html.twig', [
-            'quotes' => $quotes,
-        ]);
-    }
-
-    #[Route('/rapports', name: 'app_reports')]
-    public function reports(): Response
-    {
-        return $this->render('dashboard/reports.html.twig');
+        return $this->render('dashboard/index.html.twig');
     }
 
 }
--- src/Controller/User/InvoiceController.php
+++ src/Controller/User/InvoiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\User;
 
+use App\Entity\Company;
 use App\Entity\Invoice;
 use App\Form\Invoice\InvoiceFormType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -9,22 +10,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class InvoiceController extends AbstractController
 {
-    #[Route('/invoice', name: 'app_user_invoice_index')]
-    public function list(EntityManagerInterface $entityManager): Response
+    #[Route('{slug}/invoice', name: 'app_user_invoice_index')]
+    public function list(EntityManagerInterface $entityManager, Company $company): Response
     {
-        $users = $this->getUser();
-        $invoices = $entityManager->getRepository(Invoice::class)->findBy([$users]);
+
+        $invoices = $entityManager->getRepository(Invoice::class)->findBy(['company' => $company]);
 
         return $this->render('invoices/invoice_index.html.twig', [
             'invoices' => $invoices,
+            'company' => $company,
         ]);
     }
 
-    #[Route('/add', name: 'app_user_invoice_add', methods: ['GET', 'POST'])]
-    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('{company_slug}/invoice/{id}', name: 'app_user_invoice_show', methods: ['GET'])]
+    public function show(Invoice $invoice): Response
+    {
+        return $this->render('invoices/invoice_show.html.twig', [
+            'invoice' => $invoice,
+        ]);
+    }
+
+    #[Route('{slug}/add', name: 'app_user_invoice_add', methods: ['GET', 'POST'])]
+    public function add(Request $request, EntityManagerInterface $entityManager, Company $company): Response
     {
         $invoice = new Invoice();
         $form = $this->createForm(InvoiceFormType::class, $invoice);
@@ -41,11 +52,14 @@ class InvoiceController extends AbstractController
             'action' => 'Ajouter une facture',
             'invoice' => $invoice,
             'form' => $form,
+            'company' => $company,
         ]);
     }
 
-    #[Route('invoice/edit/{id}', name: 'app_user_invoice_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Invoice $invoice, EntityManagerInterface $entityManager): Response
+    #[Route('{company_slug}/invoice/edit/{id}', name: 'app_user_invoice_edit', methods: ['GET', 'POST'])]
+    #[ParamConverter('invoice', class: Invoice::class, options: ['mapping' => ['id' => 'id']])]
+    #[ParamConverter('Company', class: Company::class, options: ['mapping' => ['company_slug' => 'slug']])]
+    public function edit(Request $request, Company $company, Invoice $invoice, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(InvoiceFormType::class, $invoice);
         $form->handleRequest($request);
@@ -60,17 +74,20 @@ class InvoiceController extends AbstractController
             'action' => 'Modifier la facture n°' . $invoice->getId(),
             'categories' => $invoice,
             'form' => $form,
+            'company' => $company,
         ]);
     }
 
-    #[Route('invoice/delete/{id}', name: 'app_user_invoice_delete', methods: ['POST'])]
-    public function delete(Request $request, Invoice $invoice, EntityManagerInterface $entityManager): Response
+    #[Route('{slug}/invoice/delete/{id}', name: 'app_user_invoice_delete', methods: ['POST'])]
+    public function delete(Request $request, Invoice $invoice, EntityManagerInterface $entityManager, Company $company): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$invoice->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $invoice->getId(), $request->request->get('_token'))) {
             $entityManager->remove($invoice);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_user_invoice_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_user_invoice_index', [
+            'company' => $company,
+        ], Response::HTTP_SEE_OTHER);
     }
 }
